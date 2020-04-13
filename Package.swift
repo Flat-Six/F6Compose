c268f07539c326230f307e039721b8c7343af615
Kyle Cronin <kyle.cronin@gmail.com>
2020-04-12 21:37:31 -0700

    Switch to dynamic type

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "F6Compose",
+            type: .dynamic,
             targets: ["F6Compose"]),
     ],
     dependencies: [
